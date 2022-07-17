9f0e510f800a694eae5a71e52d40fb5ffbb2b4b7
Nikolas Sanchez <yoshiniko1223@gmail.com>
2022-07-16 18:20:33 -0700

    Added 2 more characters, and also mobile support
    I'll figure out mobile support later but
    
    2 characters were made and added in woo

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour{
+
+    #region Public Variables
+    [Header("Game Objects")]
+
+    [Tooltip("PlayerPiece that will move")]
+    public GameObject PlayerPiece;
+
+    [Tooltip("The Slider that will move the game piece")]
+    public Slider GamePieceSlider;
+
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
